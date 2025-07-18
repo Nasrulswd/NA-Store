958bdcea1775bdcb978ceed7f1400b6d8b132ad4
Nasrul Aswad <nxsrulswd@gmail.com>
2025-07-18 14:02:52 +0800

    Delete progmega directory

--- progmega/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$host = "localhost";
-	$user = "root";
-	$password = "";
-	$database = "fn3ducate_db";
-	
-	$connection = mysqli_connect($host, $user, $password, $database)
-	or die('Connection Failed! Please recheck!');
-?>
